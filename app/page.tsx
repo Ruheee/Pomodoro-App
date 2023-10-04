d9beabfb71935d2c576ebf0f49aa537cbbcb78e1
ruheee <Ruidantas55@gmail.com>
2023-10-04 13:17:48 -0400

    ensure sign-in/up work

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 
+import { UserButton } from '@clerk/nextjs'
+
 export default function Home() {
   return (
     <main className="min-h-screen bg-red-500">
+      <UserButton afterSignOutUrl="/"/>
       <p>Hello</p>
     </main>
   )
 }
+
+
