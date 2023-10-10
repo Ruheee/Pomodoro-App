9a4ed709aa713ca69d88d69cfb4c6bf756d2ed57
ruheee <Ruidantas55@gmail.com>
2023-10-10 15:50:33 -0400

    Get timer to start, pause and reset

--- app/components/counter.tsx
+++ app/components/counter.tsx
@@ -1,7 +1,46 @@
 "use client";
 
-const Counter = ({ stage, setTimer, switchStage }) => {
+import { useEffect, useState } from "react";
+
+const Counter = () => {
   const options = ["Pomodoro", "Short Break", "Long Break"];
+  const [time, setTime] = useState(25 * 60); // Initial time in seconds (25 minutes)
+  const [isActive, setIsActive] = useState(false);
+  const [isBreak, setIsBreak] = useState(false);
+
+  useEffect(() => {
+    let interval;
+
+    if (isActive && time > 0) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime - 1);
+      }, 1000);
+    } else if (isActive && time === 0) {
+      setIsActive(false);
+      setIsBreak(!isBreak);
+      setTime(isBreak ? 25 * 60 : 5 * 60); // Switch between work and break time
+    } else {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [isActive, time, isBreak]);
+
+  const formatTime = (seconds) => {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${minutes}:${remainingSeconds < 10 ? "0" : ""}${remainingSeconds}`;
+  };
+
+  const toggleTimer = () => {
+    setIsActive(!isActive);
+  };
+
+  const resetTimer = () => {
+    setIsActive(false);
+    setIsBreak(false); // Reset to work time
+    setTime(25 * 60); // Reset to 25 minutes (or your desired initial time)
+  };
 
   return (
     <div>
@@ -10,21 +49,31 @@ const Counter = ({ stage, setTimer, switchStage }) => {
           {options.map((option, index) => {
             return (
               <button
-            className="border-none m-0 rounded-[4px] p-[2px 12px] h-7 cursor-pointer bg-none text-white opacity-100 font-bold w-[100px] focus:bg-white/25"
-            key={index}
-            onClick={() => switchStage(index)}
-          >
-            {option}
-          </button>
-            )
+                className="border-none m-0 rounded-[4px] p-[2px 12px] h-7 cursor-pointer bg-none text-white opacity-100 font-bold w-[100px] focus:bg-white/25"
+                key={index}
+              >
+                {option}
+              </button>
+            );
           })}
         </div>
         <div>
-          <p className="font-bold text-white text-[98px]">{setTimer()}:00</p>
+          <p className="font-bold text-white text-[98px]">{formatTime(time)}</p>
         </div>
-        <button className="text-[#BA4949] text-[24px] bg-white px-14 py-2 rounded-md mt-2 drop-shadow-xl uppercase font-bold">
-          Start
-        </button>
+        <div className="flex">
+          <button
+            className="text-[#BA4949] text-[24px] bg-white px-14 py-2 rounded-md mt-2 drop-shadow-xl uppercase font-bold mr-2"
+            onClick={toggleTimer}
+          >
+            {isActive ? "Pause" : "Start"}
+          </button>
+          <button
+            className="text-[#BA4949] text-[24px] bg-white px-14 py-2 rounded-md mt-2 drop-shadow-xl uppercase font-bold"
+            onClick={resetTimer}
+          >
+            Reset
+          </button>
+        </div>
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -2,31 +2,15 @@
 
 import NavBar from "@/app/components/navbar";
 import Counter from "@/app/components/counter";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [pomodoro, setPomodoro] = useState(25);
-  const [shortBreak, setShortBreak] = useState(5);
-  const [longBreak, setLongBreak] = useState(10);
-  const [stage, setStage] = useState(0);
 
-  const switchStage = (index) => {
-    setStage(index)
-  }
-
-  const setTimer = () => {
-    const timer = {
-      0: pomodoro,
-      1: shortBreak,
-      2: longBreak
-    }
-    return timer[stage];
-  }
 
   return (
     <main className="min-h-screen bg-[#BA4949] px-[600px] box-border m-0 p-0">
       <NavBar />
-      <Counter stage={stage} setTimer={setTimer} switchStage={switchStage}/>
+      <Counter />
     </main>
   );
 }
