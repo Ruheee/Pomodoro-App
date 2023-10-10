20b3c8281a062b32db3de2e77ac6a9b05835a116
ruheee <Ruidantas55@gmail.com>
2023-10-10 16:38:08 -0400

    Add the timer logic for the break times

--- app/components/counter.tsx
+++ app/components/counter.tsx
@@ -3,10 +3,15 @@
 import { useEffect, useState } from "react";
 
 const Counter = () => {
-  const options = ["Pomodoro", "Short Break", "Long Break"];
+  const [selectedOption, setSelectedOption] = useState("Pomodoro");
   const [time, setTime] = useState(25 * 60); // Initial time in seconds (25 minutes)
   const [isActive, setIsActive] = useState(false);
-  const [isBreak, setIsBreak] = useState(false);
+
+  const options = {
+    Pomodoro: 25 * 60,
+    "Short Break": 5 * 60,
+    "Long Break": 15 * 60,
+  };
 
   useEffect(() => {
     let interval;
@@ -17,14 +22,16 @@ const Counter = () => {
       }, 1000);
     } else if (isActive && time === 0) {
       setIsActive(false);
-      setIsBreak(!isBreak);
-      setTime(isBreak ? 25 * 60 : 5 * 60); // Switch between work and break time
+      const nextOption =
+        selectedOption === "Pomodoro" ? "Short Break" : "Pomodoro";
+      setSelectedOption(nextOption);
+      setTime(options[nextOption]);
     } else {
       clearInterval(interval);
     }
 
     return () => clearInterval(interval);
-  }, [isActive, time, isBreak]);
+  }, [isActive, time, selectedOption, options]);
 
   const formatTime = (seconds) => {
     const minutes = Math.floor(seconds / 60);
@@ -36,21 +43,24 @@ const Counter = () => {
     setIsActive(!isActive);
   };
 
-  const resetTimer = () => {
+  const handleOptionChange = (newOption) => {
+    setSelectedOption(newOption);
+    setTime(options[newOption]);
     setIsActive(false);
-    setIsBreak(false); // Reset to work time
-    setTime(25 * 60); // Reset to 25 minutes (or your desired initial time)
   };
 
   return (
     <div>
-      <div className="bg-white/25 w-1/2  m-auto mt-5 rounded-[20px] py-[50px]  px-[250px] flex flex-col items-center text-[100px]">
+      <div className="bg-white/25 w-1/2 m-auto mt-5 rounded-[20px] py-[50px] px-[250px] flex flex-col items-center text-[100px]">
         <div className="text-[16px] flex">
-          {options.map((option, index) => {
+          {Object.keys(options).map((option, index) => {
             return (
               <button
-                className="border-none m-0 rounded-[4px] p-[2px 12px] h-7 cursor-pointer bg-none text-white opacity-100 font-bold w-[100px] focus:bg-white/25"
+                className={`border-none m-0 rounded-[4px] p-[2px 12px] h-7 cursor-pointer bg-none text-white opacity-100 font-bold w-[100px] focus:bg-white/25 ${
+                  option === selectedOption ? "bg-white/25" : ""
+                }`}
                 key={index}
+                onClick={() => handleOptionChange(option)}
               >
                 {option}
               </button>
@@ -69,7 +79,7 @@ const Counter = () => {
           </button>
           <button
             className="text-[#BA4949] text-[24px] bg-white px-14 py-2 rounded-md mt-2 drop-shadow-xl uppercase font-bold"
-            onClick={resetTimer}
+            onClick={() => handleOptionChange("Pomodoro")}
           >
             Reset
           </button>
@@ -80,3 +90,4 @@ const Counter = () => {
 };
 
 export default Counter;
+
