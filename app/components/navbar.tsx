b8aa664bb0ea966d8ec7d8edb940f64b709a9164
ruheee <Ruidantas55@gmail.com>
2023-10-16 14:41:11 -0400

    Add Spotify log in ability

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,7 +1,7 @@
 import { GrMusic } from "react-icons/gr";
 
 const NavBar = () => {
-  const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=b9b075e7f9a345a19d3f1a0d1b20385b&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-read-playback-state%20playlist-read-private%20playlist-read-collaborative%20playlist-modify-public"
+  const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=b9b075e7f9a345a19d3f1a0d1b20385b&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-read-playback-state%20playlist-read-private%20playlist-read-collaborative%20playlist-modify-public"  
   return (
     <nav className=" w-full pt-10 border-b-[0.5px] border-zinc-700 text-white">
       <div className="flex justify-between pb-2">
