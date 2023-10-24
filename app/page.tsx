198954c0ed1cfe58959fa6f2769c7d604e2982d9
ruheee <Ruidantas55@gmail.com>
2023-10-24 12:54:43 -0400

    Complete the pomodoro aspet of app

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,14 @@
 "use client";
-
 import NavBar from "@/app/components/navbar";
 import Counter from "@/app/components/counter";
-import Player from "@/app/components/player";
 
 
 export default function Home() {
-
 
   return (
     <main className="min-h-screen bg-[#BA4949] px-[600px] box-border m-0 p-0">
       <NavBar />
       <Counter />
-      <Player />  
     </main>
   );
 }
